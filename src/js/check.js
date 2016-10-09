61fa61b9e533756e74317c4d0a1cfe4acfd65812
Akramov Sardor <akramovss@gmail.com>
2016-10-09 06:30:45 +0300

    1ый раздел. До проверки

---
+++ src/js/check.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var getMessage = function (a, b) {
+  if (typeof a == "boolean") {
+    if (a == true) {return "Я попал в " + [b];}
+    else {
+      return "Я никуда не попал";
+    }
+  }
+  
+  if (typeof a == "number") {
+    return "Я прыгнул на " + [a] * 100 + " сантиметров";
+  }
+
+  if (Array.isArray(a)) {
+    var numberOfSteps = 0;
+
+    for (var i = 0; i < a.length; i++) {
+      numberOfSteps = numberOfSteps + a[i];
+    }
+    return "Я прошёл " + [numberOfSteps] + " шагов";
+  }
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    var distancePath = 0;
+
+    for (var i = 0; i < a.length; i++) {
+      distancePath = distancePath + a[i] * b[i];
+    }
+    return "Я прошёл " + [distancePath] + " метров";
+  }
+
+  else {
+    return "Переданы некорректные данные";
+  }
+}
