df8c15c62075d918f53d438d3bc229f9454e23aa
Akramov Sardor <akramovss@gmail.com>
2016-10-11 01:56:20 +0300

    1 раздел. Финал

--- src/js/check.js
+++ src/js/check.js
@@ -1,18 +1,18 @@
 'use strict';
 
-var getMessage = function (a, b) {
+var getMessage = function(a, b) {
   if (typeof a === 'boolean') {
     if (a) {
       return 'Я попал в ' + b;
     } else {
-        return 'Я никуда не попал';
+      return 'Я никуда не попал';
     }
   } else if (typeof a === 'number') {
-  return 'Я прыгнул на ' + a * 100 + ' сантиметров';
+    return 'Я прыгнул на ' + a * 100 + ' сантиметров';
   } else if (Array.isArray(a) && !Array.isArray(b)) {
-      var numberOfSteps = 0;
+    var numberOfSteps = 0;
     for (var i = 0; i < a.length; i++) {
-    numberOfSteps += a[i];
+      numberOfSteps += a[i];
     }
     return 'Я прошёл ' + numberOfSteps + ' шагов';
   } else if (Array.isArray(a) && Array.isArray(b)) {
@@ -21,6 +21,6 @@ var getMessage = function (a, b) {
       distancePath += a[j] * b[j];
     }
     return 'Я прошёл ' + distancePath + ' метров';
-    }
+  }
   return 'Переданы некорректные данные';
 };
