c85200e70bb8eb17420008d56f60bf2887f4bf1d
Akramov Sardor <akramovss@gmail.com>
2016-10-11 00:44:15 +0300

    1ый раздел. 6 правка

--- src/js/check.js
+++ src/js/check.js
@@ -1,30 +1,26 @@
 'use strict';
 
 var getMessage = function (a, b) {
-  if (typeof a == 'boolean') {
+  if (typeof a === 'boolean') {
     if (a) {
-      return 'Я попал в ' + [b];
+      return 'Я попал в ' + b;
     } else {
         return 'Я никуда не попал';
-      }
-  } else
-  if (typeof a == 'number') {
-    return 'Я прыгнул на ' + [a] * 100 + ' сантиметров';
-  } else
-  if (Array.isArray(a) && !Array.isArray(b)) {
-    var numberOfSteps = 0;
-    for (var i = 0; i < a.length; i++) {
-    numberOfSteps = numberOfSteps + a[i];
     }
-    return 'Я прошёл ' + [numberOfSteps] + ' шагов';
-  } else
-  if (Array.isArray(a) && Array.isArray(b)) {
+  } else if (typeof a === 'number') {
+  return 'Я прыгнул на ' + a * 100 + ' сантиметров';
+  } else if (Array.isArray(a) && !Array.isArray(b)) {
+      var numberOfSteps = 0;
+    for (var i = 0; i < a.length; i++) {
+    numberOfSteps += a[i];
+    }
+    return 'Я прошёл ' + numberOfSteps + ' шагов';
+  } else if (Array.isArray(a) && Array.isArray(b)) {
     var distancePath = 0;
     for (var j = 0; j < a.length; j++) {
-      distancePath = distancePath + a[j] * b[j];
+      distancePath += a[j] * b[j];
     }
-    return 'Я прошёл ' + [distancePath] + ' метров';
-  } else {
-    return 'Переданы некорректные данные';
-  }
+    return 'Я прошёл ' + distancePath + ' метров';
+    }
+  return 'Переданы некорректные данные';
 };
