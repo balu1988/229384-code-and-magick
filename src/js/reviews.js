e6b7ffcc10de417a2545ee0e91411d2d4f5ebc24
Akramov Sardor <akramovss@gmail.com>
2016-11-13 21:11:07 +0300

    5-1. Модули. Правки

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,19 +1,17 @@
 'use strict';
 
-module.exports = (function() {
-  var getJSONPData = require('./load.js');
-  var getReviewElement = require('./review.js');
+var getJSONPData = require('./load.js');
+var getReviewElement = require('./review.js');
 
-  var reviewsFilter = document.querySelector('.reviews-filter'); // блок с фильтрами
-  reviewsFilter.classList.add('invisible'); // прячет блок с фильтрами
+var reviewsFilter = document.querySelector('.reviews-filter'); // блок с фильтрами
+reviewsFilter.classList.add('invisible'); // прячет блок с фильтрами
 
-  var container = document.querySelector('.reviews-list'); // сюда копируем созданные элементы
+var container = document.querySelector('.reviews-list'); // сюда копируем созданные элементы
 
-  getJSONPData('http://localhost:1507/api/reviews?callback=JSONPCallback', function(reviews) {
-    reviews.forEach(function(review) {
-      container.appendChild(getReviewElement(review));
-    });
+getJSONPData('http://localhost:1507/api/reviews?callback=JSONPCallback', function(reviews) {
+  reviews.forEach(function(review) {
+    container.appendChild(getReviewElement(review));
   });
+});
 
-  reviewsFilter.classList.remove('invisible'); // показывает блок с фильтрами
-})();
+reviewsFilter.classList.remove('invisible'); // показывает блок с фильтрами
