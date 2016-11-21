daf07460d9e55831041e9eda8316ffd33112853e
Akramov Sardor <akramovss@gmail.com>
2016-11-21 13:04:33 +0300

    7-1. 21 задание

--- src/js/load.js
+++ src/js/load.js
@@ -1,12 +1,28 @@
 'use strict';
 
-module.exports = function(url, callback) {
-  var elem = document.createElement('script');
-  elem.src = url;
-  document.head.appendChild(elem);
+// module.exports = function(url, callback) {
+//   var elem = document.createElement('script');
+//   elem.src = url;
+//   document.head.appendChild(elem);
+//
+//   window.JSONPCallback = function(data) {
+//     window.reviews = data;
+//     callback(window.reviews);
+//   };
+// };
 
-  window.JSONPCallback = function(data) {
-    window.reviews = data;
-    callback(window.reviews);
+module.exports = (function(url, params, callback) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onload = function(evt) {
+    var reviews = JSON.parse(evt.target.response);
+    callback(reviews);
   };
-};
+
+  xhr.open('GET', url +
+   '?from=' + (params.from || 0) +
+   '&to=' + (params.to || Infinity) +
+   '&filter=' + (params.filter || 'default'));
+
+  xhr.send();
+});
